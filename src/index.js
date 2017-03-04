3d1d39f8f2981501f717753dda32e39e0ba1c23b
Daniel Walker <polyesterhat@gmail.com>
2017-03-04 16:17:02 -0500

    remove unused css

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './index.css';
 
 var root = document.createElement('div');
 root.setAttribute('id', 'root');
 document.body.appendChild(root);
-console.log('asdfasdf')
+
 ReactDOM.render(
   <App />,
   root
